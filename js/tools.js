8119ab549a2ab1baf4e9c414eb4b9cfdf971a709
reneang17 <reneang17@gmail.com>
2024-04-22 23:27:32 -0600

    All

--- js/tools.js
+++ js/tools.js
@@ -12,6 +12,49 @@ function generateRandomLink() {
     // Update the href attribute of the link and modify the link text
     const link = document.getElementById('randomPageLink');
     link.href = url;
-    link.textContent = `Link to page ${(randomPage - 21)}`;
+    link.textContent = `Link to page ${(randomPage - 18)}`;
     link.style.display = 'inline'; // Make the link visible
+}
+
+async function fetchVerseDetails() {
+    const verseInput = document.getElementById('verseNumber');
+    const verseId = verseInput.value;
+    if (verseId < 1 || verseId > 423) {
+        alert('Please enter a valid verse number between 1 and 423.');
+        return;
+    }
+
+    try {
+        const response = await fetch('./ajahn_munindo_books/data.json');
+        if (!response.ok) {
+            throw new Error('Failed to fetch verse details.');
+        }
+        const verses = await response.json();
+        console.log('gato')
+        displayVerseDetails(verses.find(verse => verse.id === parseInt(verseId)));
+    } catch (error) {
+        console.error('Error fetching verse details:', error);
+        alert('Error fetching data. Please try again.');
+    }
+}
+
+
+
+
+function displayVerseDetails(verse) {
+    if (!verse) {
+        alert('Verse details not found.');
+        return;
+    }
+
+    const detailsContainer = document.getElementById('verseDetails');
+    detailsContainer.innerHTML = `
+        <p>${verse.utter}</p>
+        <p>The historical context of this verse can be found in:</p>
+        <ul>${verse.context.map(url => `<li><a href="${url}" target="_blank">${url}</a></li>`).join('')}</ul>
+        <p>You can find Ajahn Munindo's commentaries at:</p>
+        <ul>${verse.refs.map((ref, index) =>
+        `<li><a href="${verse.urls[index]}" target="_blank">${ref}</a></li>`).join('')}
+        </ul>
+    `;
 }
