4b180f165df14fa5b594602ca6278b06f71f591b
reneang17 <reneang17@gmail.com>
2024-04-23 21:27:56 -0600

    Added maitri

--- js/teachings.js
+++ js/teachings.js
@@ -21,3 +21,28 @@ async function generateRandomSanghaLink() {
         alert('Error fetching links. Please try again.');
     }
 }
+
+
+async function generateRandomMaitriLink() {
+    try {
+        const response = await fetch('./open_sangha_and_maitri/maitri.json');  // Ensure the path matches your JSON file location
+        if (!response.ok) {
+            throw new Error('Failed to fetch maitri links.');
+        }
+        const links = await response.json();
+        const randomIndex = Math.floor(Math.random() * links.length);
+        const randomLink = links[randomIndex].maitri;
+
+        const linkContainer = document.getElementById('linkContainer');
+        linkContainer.innerHTML = '';  // Clear previous links if any
+
+        const linkElement = document.createElement('a');
+        linkElement.href = randomLink;
+        linkElement.textContent = 'Maitri link';
+        linkElement.target = '_blank';
+        linkContainer.appendChild(linkElement);
+    } catch (error) {
+        console.error('Error fetching maitri links:', error);
+        alert('Error fetching links. Please try again.');
+    }
+}
