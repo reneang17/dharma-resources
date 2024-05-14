3db1ac9e54114ce88abd321971ee3b3ac9d0855d
reneang17 <reneang17@gmail.com>
2024-05-14 12:41:33 -0600

    Working milestone, with buttons fixed. Modified data.json

--- js/tools.js
+++ js/tools.js
@@ -7,7 +7,7 @@ function generateRandomLink() {
     const randomPage = Math.floor(Math.random() * (maxPage - minPage + 1)) + minPage;
 
     // Construct the URL with the random page
-    const url = `https://reneang17.github.io/dharma-resources/ajahn_munindo_books/dhammapada-for-contemplation-5th-LARGE-WEB-2017-02-05.pdf#page=${randomPage}`;
+    const url = `./ajahn_munindo_books/dhammapada-for-contemplation-5th-LARGE-WEB-2017-02-05.pdf#page=${randomPage}`;
 
     // Update the href attribute of the link and modify the link text
     const link = document.getElementById('randomPageLink');
