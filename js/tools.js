ff4e9e140ae1c519989b7d7ebc4d30afea03b0ae
reneang17 <reneang17@gmail.com>
2024-06-01 15:41:53 -0600

    Toggle switch added

--- js/tools.js
+++ js/tools.js
@@ -41,9 +41,9 @@ function displayVerseDetails(verse) {
         const url = verse.urls[index];
         const viewerId = `pdf-viewer-${index}`;
         if (url.includes('.pdf')) {
-            htmlContent += `<li><button onclick="loadPDF('${url}', '${viewerId}')">${ref}</button><div id="${viewerId}" class="pdf-viewer-container"></div></li>`;
+            htmlContent += `<li>${ref} <label class="switch toggle-button"><input type="checkbox" onchange="togglePDF(this.checked, '${url}', '${viewerId}')"><span class="slider round"></span></label><div id="${viewerId}" class="pdf-viewer-container" style="display:none;"></div></li>`;
         } else {
-            htmlContent += `<li><a href="${url}" target="_blank">${ref}</a></li>`;
+            htmlContent += `<li>${ref} <a href="${url}" target="_blank">(Link)</a></li>`;
         }
     });
 
@@ -51,13 +51,25 @@ function displayVerseDetails(verse) {
     detailsContainer.innerHTML = htmlContent;
 }
 
-// Function to instantiate and load a PDF viewer
+
+// Function to toggle the PDF viewer based on switch state
+function togglePDF(isChecked, url, containerId) {
+    if (isChecked) {
+        loadPDF(url, containerId);
+    } else {
+        hidePDF(containerId);
+    }
+}
+
+// Function to load the PDF viewer
 function loadPDF(url, containerId) {
     const pageMatch = url.match(/#page=(\d+)/);
     const pageNum = pageMatch ? pageMatch[1] : 1;
     new CommentsViewer(containerId, url, pageNum).init();
+    document.getElementById(containerId).style.display = 'block';
 }
 
-
-
-
+// Function to hide the PDF viewer
+function hidePDF(containerId) {
+    document.getElementById(containerId).style.display = 'none';
+}
