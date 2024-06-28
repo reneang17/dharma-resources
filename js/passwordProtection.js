4241c8e5db0d64b6137ccac52d1549aa4ddb14a0
reneang17 <reneang17@gmail.com>
2024-06-28 00:13:01 -0600

    Final kindness

--- js/passwordProtection.js
+++ js/passwordProtection.js
@@ -1,9 +1,13 @@
+
+
 function checkPassword() {
     const passwordInput = document.getElementById('password').value;
-    const correctPasswords = ['be kind to yourself', 'gate']; // Add your list of passwords here
+    const correctPasswords = ['be kind to yourself', 'kind', 'gate']; // Add your list of passwords here
+    const content = document.getElementById('protected-content');
 
     if (correctPasswords.includes(passwordInput)) {
-        alert('Password is correct.');
+        content.style.display = 'block';
+        document.getElementById('password-section').style.display = 'none';
     } else {
         alert('Incorrect password. Please try again.');
     }
