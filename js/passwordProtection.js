e5381403dbbc327a08d4243bb7190d04c0271ddd
reneang17 <reneang17@gmail.com>
2024-06-27 22:24:26 -0600

    added kindness password

--- js/passwordProtection.js
+++ js/passwordProtection.js
@@ -1,11 +1,9 @@
 function checkPassword() {
     const passwordInput = document.getElementById('password').value;
     const correctPasswords = ['be kind to yourself', 'gate']; // Add your list of passwords here
-    const content = document.getElementById('protected-content');
 
     if (correctPasswords.includes(passwordInput)) {
-        content.style.display = 'block';
-        document.getElementById('password-section').style.display = 'none';
+        alert('Password is correct.');
     } else {
         alert('Incorrect password. Please try again.');
     }
